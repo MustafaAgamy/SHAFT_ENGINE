42fe9616f9b4027234c3601259f821f2f3c828ed
MustafaAgamy <mostafaagamy1995@gmail.com>
2024-02-08 02:11:04 +0200

    Add files for testing Google Ads

--- src/test/java/testPackage/unitTests/TestGoogleAds.java
+++ src/test/java/testPackage/unitTests/TestGoogleAds.java
@@ -101,7 +101,7 @@ public class TestGoogleAds {
 
     /////////////////// Utilities \\\\\\\\\\\\\\\\\\\\
     public TestGoogleAds dismissAlert(SHAFT.GUI.WebDriver driver){
-        new Actions(driver.getDriver()).pause(1500).doubleClick(driver.getDriver().findElement(By.tagName("header"))).perform();
+        new Actions(driver.getDriver()).doubleClick(driver.getDriver().findElement(By.tagName("header"))).perform();
         return this;
     }
 }
